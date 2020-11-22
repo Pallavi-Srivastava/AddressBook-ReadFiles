e21414a938e5ff9d42d0b3b3de05b4b2fd0b9077
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-11-22 13:59:17 +0530

    Added:Code for update the contact info.

---
+++ src/main/java/com/blz/addressbook/AddressBookService.java
@@ -0,0 +1,46 @@
+package com.blz.addressbook;
+
+import java.util.List;
+
+public class AddressBookService {
+
+	public enum IOService {
+		DB_IO
+	}
+
+	private List<PersonDetails> addressBookList;
+	private static AddressBookDBService addressBookDBService;
+
+	public AddressBookService() {
+		addressBookDBService = AddressBookDBService.getInstance();
+	}
+
+	public List<PersonDetails> readAddressBookData(IOService ioService) throws AddressBookException {
+		if (ioService.equals(IOService.DB_IO))
+			return this.addressBookList = addressBookDBService.readData();
+		return this.addressBookList;
+	}
+
+	public void updateRecord(String firstname, String address) throws AddressBookException {
+		int result = addressBookDBService.updateAddressBookData(firstname, address);
+		if (result == 0)
+			return;
+		PersonDetails addressBookData = this.getAddressBookData(firstname);
+		if (addressBookData != null)
+			addressBookData.address = address;
+	}
+
+	public boolean checkUpdatedRecordSyncWithDatabase(String firstname) throws AddressBookException {
+		try {
+			List<PersonDetails> addressBookData = addressBookDBService.getAddressBookData(firstname);
+			return addressBookData.get(0).equals(getAddressBookData(firstname));
+		} catch (AddressBookException e) {
+			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.DatabaseException);
+		}
+	}
+
+	private PersonDetails getAddressBookData(String firstname) {
+		return this.addressBookList.stream().filter(addressBookItem -> addressBookItem.firstName.equals(firstname))
+				.findFirst().orElse(null);
+	}
+}
---
+++ src/main/java/com/blz/addressbook/PersonDetails.java
@@ -0,0 +1,142 @@
+package com.blz.addressbook;
+
+public class PersonDetails {
+
+	String firstName;
+	String lastName;
+	String address;
+	String city;
+	String state;
+	int zip;
+	int phoneNo;
+	String email;
+
+	@Override
+	public String toString() {
+		return "AddressBook [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city="
+				+ city + ", state=" + state + ", zip=" + zip + ", phoneNo=" + phoneNo + ", email=" + email + "]";
+	}
+
+	public PersonDetails(String firstName, String lastName, String address, String city, String state, int zip,
+			int phoneNo, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNo = phoneNo;
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public int getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(int phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonDetails other = (PersonDetails) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (phoneNo != other.phoneNo)
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (zip != other.zip)
+			return false;
+		return true;
+	}
+}
